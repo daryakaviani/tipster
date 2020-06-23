569dcd44da69b729861b3b1d598a720330b8b18c
Darya Kaviani <dkaviani@fb.com>
2020-06-23 13:13:18 -0700

    Initial Commit

---
+++ tipster/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipster
+//
+//  Created by dkaviani on 6/23/20.
+//  Copyright © 2020 dkaviani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ tipster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipster
+//
+//  Created by dkaviani on 6/23/20.
+//  Copyright © 2020 dkaviani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
